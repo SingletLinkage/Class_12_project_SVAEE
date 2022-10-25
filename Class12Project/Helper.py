d625b4cadf998c1d7ea8dd1ac66340324e6dbe85
Singlet Linkage <96745221+SingletLinkage@users.noreply.github.com>
2022-10-25 11:44:40 +0530

    Update Helper.py
    added code to create database and initialize tables for the first time

--- Class12Project/Helper.py
+++ Class12Project/Helper.py
@@ -2,15 +2,14 @@ import pandas as pd
 import mysql.connector
 import time
 
+DATABASE = 'ip_project'
+USERNAME = 'root'
+PASSWORD = 'root'
+
 
 def execute_sql(query):
     # actually for executing general query -- DOES NOT return anything
-    conn = mysql.connector.connect(
-        host='localhost',
-        user='root',
-        passwd='root',
-        database='IPproject_test'
-    )
+    conn = setup_connection_to_database()
     cursor = conn.cursor()
 
     if conn.is_connected():
@@ -24,12 +23,7 @@ def execute_sql(query):
 
 def get_data_from_table(query):
     # actually for reading sql and returning output
-    conn = mysql.connector.connect(
-        host='localhost',
-        user='root',
-        passwd='root',
-        database='IPproject_test'
-    )
+    conn = setup_connection_to_database()
 
     if conn.is_connected():
         return pd.read_sql(query, conn)
@@ -37,6 +31,29 @@ def get_data_from_table(query):
         print('Could not connect to sql database')
 
     conn.close()
+
+
+def setup_connection_to_database():
+    if not check_if_exists(DATABASE):
+        init_database(DATABASE)
+
+    connection = mysql.connector.connect(
+        host='localhost',
+        user=USERNAME,
+        password=PASSWORD,
+        database=DATABASE
+    )
+    return connection
+
+
+def setup_general_connection():
+    connection = mysql.connector.connect(
+        host='localhost',
+        user=USERNAME,
+        password=PASSWORD,
+    )
+
+    return connection
 
 
 def add_delay():
@@ -48,3 +65,34 @@ def add_delay():
     print()
     print('-' * 70)
 
+
+def init_database(database_name):
+    connection = setup_general_connection()
+    cursor = connection.cursor()
+    cursor.execute(f'create database if not exists {database_name};')
+    cursor.execute(f'use {database_name};')
+    cursor.execute('''
+    create table pack_details(
+        src varchar(10),
+        dest varchar(10),
+        description char(100),
+        cost int,
+        name varchar(10),
+        id int(10) primary key);''')
+
+    cursor.execute('''
+    create table user_list(
+        first_name varchar(10),
+        last_name varchar(10),
+        username varchar(20) primary key,
+        password varchar(20),
+        active_packs varchar(120),
+        type varchar(10));''')
+    cursor.execute('insert into user_list values("admin1", "admin1", "admin", "admin", "", "ADMIN");')
+    cursor.execute('commit;')
+
+
+def check_if_exists(database):
+    connection = setup_general_connection()
+    all_databases = pd.read_sql('show databases;', connection).Database
+    return database.lower() in all_databases.values
